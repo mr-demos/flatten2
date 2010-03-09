dc44efe7862be08646422d22794bcb86532c83cc
Matt Royal <mroyal@gmail.com>
2010-03-09 00:30:38 -0500

    Some final cleanup on the Flatten2::Array module.

--- lib/flatten2/array.rb
+++ lib/flatten2/array.rb
@@ -3,13 +3,18 @@ class Flatten2
     def flatten2
       flattened = []
       self.each do |entry|
-        if entry.is_a? Array # could do entry.respond_to?(:flatten2), but then you get weird behavior if the class' implementation of flatten is intended for something else
+        if entry.is_a? Array
+          # could duck type with entry.respond_to?(:flatten2), but then you get
+          # weird behavior if the class' implementation of flatten2 is intended
+          # for something else
           flattened += entry.flatten2
         else
           flattened << entry
         end
       end
-      # this could be an implicit return, but I prefer explicit returns for methods longer than 1 line, just to make it explicit that the value is expected by clients
+      # this could be an implicit return, but I prefer explicit returns for
+      # methods longer than 1 line, just to make it explicit that the value is
+      # expected by clients
       return flattened
     end
   end
